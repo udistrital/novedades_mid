8b4cb650abb0f5e573e643a56b552639e3235767
Brayanb24 <babermudezb@correo.udistrital.edu.co>
2019-09-02 17:39:13 -0500

    Añadiendo controladores de respuestas y agregando manejo de errores #2

--- controllers/registronovedad.go
+++ controllers/registronovedad.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/astaxie/beego"
+	"github.com/udistrital/novedades_mid/models"
+	"github.com/udistrital/utils_oas/time_bogota"
 )
 
 // RegistroNovedadController operations for RegistroNovedad
@@ -26,5 +31,35 @@ func (c *RegistroNovedadController) URLMapping() {
 // @Failure 403 body is empty
 // @router / [post]
 func (c *RegistroNovedadController) PostRegistroNovedad() {
+	var registroNovedad map[string]interface{}
+	var alertErr models.Alert
+	alertas := append([]interface{}{"Response:"})
+	horaRegistro := time_bogota.Tiempo_bogota()
+
+	//fmt.Println(registroNovedad, alertErr, horaRegistro)
+
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &registroNovedad); err == nil {
+
+		registroNovedadPost := make(map[string]interface{})
+		registroNovedadPost["registroNovedad"] = map[string]interface{}{
+			"Novedad":       registroNovedad["Novedad"],
+			"TipoNovedad":   registroNovedad["TipoNovedad"],
+			"Contrato":      registroNovedad["Contrato"],
+			"FechaRegistro": horaRegistro,
+		}
+
+		fmt.Println(registroNovedadPost, horaRegistro)
+		alertErr.Type = "OK"
+		alertErr.Code = "200"
+		alertas = append(alertas, registroNovedadPost)
+	} else {
+		alertErr.Type = "error"
+		alertErr.Code = "400"
+		alertas = append(alertas, err.Error())
+	}
+
+	alertErr.Body = alertas
+	c.Data["json"] = alertErr
+	c.ServeJSON()
 
 }
