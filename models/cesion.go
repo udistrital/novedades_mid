8e390706a8abd6e8a47f2ea4dd15af261e651e18
Youssef Alejandro Ortiz Vargas <42217637+Yaov05@users.noreply.github.com>
2024-11-08 17:44:38 -0500

    fix: campo entidad aseguradora

--- models/cesion.go
+++ models/cesion.go
@@ -253,8 +253,8 @@ func GetNovedadCesion(novedad map[string]interface{}) (novedadformatted map[stri
 	var cedente interface{}
 	var cesionario interface{}
 
-	// var polizas interface{}
-	// var entidadaseguradora interface{}
+	var polizas interface{}
+	var entidadaseguradora interface{}
 
 	error := request.GetJson(beego.AppConfig.String("NovedadesCrudService")+"/fechas/?query=id_novedades_poscontractuales:"+strconv.FormatFloat((NovedadAdicion["Id"]).(float64), 'f', -1, 64)+"&limit=0", &fechas)
 	error1 := request.GetJson(beego.AppConfig.String("NovedadesCrudService")+"/propiedad/?query=id_novedades_poscontractuales:"+strconv.FormatFloat((NovedadAdicion["Id"]).(float64), 'f', -1, 64)+"&limit=0", &propiedades)
